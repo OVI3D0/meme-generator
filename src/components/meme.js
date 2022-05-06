97636364b9e357a49b3282b1285130ec6586fa89
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-05-05 23:13:45 -0400

    added memesdata for now, console logging meme img url

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,7 +1,14 @@
 import React from "react";
 import '../styles/meme.css';
+import memesData from '../memesData.js';
 
 function Meme() {
+    function memeClick() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url
+        console.log(url);
+    }
     return (
         <>
             <div className="d-flex justify-content-around pt-5 mt-5">
@@ -9,7 +16,7 @@ function Meme() {
                 <input type="text" class="form-control" id="bottomText" placeholder="Bottom text" />
             </div>
             <div className="d-flex justify-content-center mt-5">
-                <button className="memebtn btn btn-primary w-50 fw-bold"><h4 className="d-inline">Get a new meme image</h4><i className="fa-solid fa-image ms-2 d-inline"></i></button>
+                <button onClick={memeClick} className="memebtn btn btn-primary w-50 fw-bold"><h4 className="d-inline">Get a new meme image</h4><i className="fa-solid fa-image ms-2 d-inline"></i></button>
             </div>
         </>
     )
