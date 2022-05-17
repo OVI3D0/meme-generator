332a64a527cee36ef1c3d773b25fff975a59a26e
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-05-17 15:55:20 -0400

    added state to display and update meme images

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import '../styles/meme.css';
 import memesData from '../memesData.js';
 
 function Meme() {
+
+    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/30b1gx.jpg");
+
     function memeClick() {
         const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNumber].url
-        console.log(url);
+        setMemeImage(memesArray[randomNumber].url)
     }
+
     return (
         <>
             <div className="d-flex justify-content-around pt-5 mt-5">
@@ -17,6 +20,9 @@ function Meme() {
             </div>
             <div className="d-flex justify-content-center mt-5">
                 <button onClick={memeClick} className="memebtn btn btn-primary w-50 fw-bold"><h4 className="d-inline">Get a new meme image</h4><i className="fa-solid fa-image ms-2 d-inline"></i></button>
+            </div>
+            <div className="d-flex justify-content-center pt-5">
+                <img src={memeImage} className="meme-img" alt="" />
             </div>
         </>
     )
