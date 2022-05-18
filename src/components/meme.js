755867058db1618f05d92096b6771f2b6a1ce188
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-05-17 22:05:17 -0400

    improved state

--- src/components/meme.js
+++ src/components/meme.js
@@ -4,12 +4,23 @@ import memesData from '../memesData.js';
 
 function Meme() {
 
-    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/30b1gx.jpg");
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = useState(memesData);
 
     function memeClick() {
-        const memesArray = memesData.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length);
-        setMemeImage(memesArray[randomNumber].url)
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
     }
 
     return (
@@ -22,7 +33,9 @@ function Meme() {
                 <button onClick={memeClick} className="memebtn btn btn-primary w-50 fw-bold"><h4 className="d-inline">Get a new meme image</h4><i className="fa-solid fa-image ms-2 d-inline"></i></button>
             </div>
             <div className="d-flex justify-content-center pt-5">
-                <img src={memeImage} className="meme-img" alt="" />
+                <p className="top-text">{meme.topText}</p>
+                <img src={meme.randomImage} className="meme-img" alt="" />
+                <p className="bottom-text">{meme.bottomText}</p>
             </div>
         </>
     )
