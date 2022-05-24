a538bf499bde047a1234a265d45a10390ff9715a
Michael Oviedo <michaeloviedo2000@gmail.com>
2022-05-24 10:23:50 -0400

    meme images now have text

--- src/components/meme.js
+++ src/components/meme.js
@@ -23,19 +23,45 @@ function Meme() {
         
     }
 
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <>
             <div className="d-flex justify-content-around pt-5 mt-5">
-                <input type="text" class="form-control" id="topText" placeholder="Top text" />
-                <input type="text" class="form-control" id="bottomText" placeholder="Bottom text" />
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    id="topText" 
+                    placeholder="Top text" 
+                    name="topText"
+                    onChange={handleChange}
+                    value={meme.topText}
+                />
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    id="bottomText" 
+                    placeholder="Bottom text" 
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={meme.bottomText}
+                />
             </div>
             <div className="d-flex justify-content-center mt-5">
                 <button onClick={memeClick} className="memebtn btn btn-primary w-50 fw-bold"><h4 className="d-inline">Get a new meme image</h4><i className="fa-solid fa-image ms-2 d-inline"></i></button>
             </div>
-            <div className="d-flex justify-content-center pt-5">
-                <p className="top-text">{meme.topText}</p>
+            <div className="d-flex justify-content-center pt-5 position-relative">
+                <h1 className="top-text fw-bolder">{meme.topText}</h1>
                 <img src={meme.randomImage} className="meme-img" alt="" />
-                <p className="bottom-text">{meme.bottomText}</p>
+                <h1 className="bottom-text fw-bolder">{meme.bottomText}</h1>
             </div>
         </>
     )
